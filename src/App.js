2a9b50e3a1094f96692eece2d0d27543dd58344d
danilomartins <dglmartins@outlook.com>
2017-07-29 00:30:43 -0400

    Save new contact to the server

--- src/App.js
+++ src/App.js
@@ -24,6 +24,17 @@ class App extends Component {
     ContactsAPI.remove(contact)
   }
 
+  //another way to add functions in a class is
+  //createContact(contact) {......whatever is in here...}
+
+  createContact = (contact) => {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState((state) => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
+
   render() {
     return (
       <div>
@@ -32,7 +43,14 @@ class App extends Component {
             onDeleteContact={this.removeContact} contacts={this.state.contacts}
           />
         )}/>
-        <Route path='/create' component={CreateContact}/>
+        <Route path='/create' render={( {history} ) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )}/>
       </div>
     )
   }
